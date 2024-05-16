8c2c4f75038fb6409246e9ab7662e3972f34b0b1
Edson Brandon <edsonbrandon8@gmail.com>
2024-05-16 12:48:05 -0300

    feat: adding fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,19 @@
+import { useFonts } from 'expo-font';
 import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
+
+  const [fontsLoaded] = useFonts({
+    NunitoSansRegular: require("../assets/fonts/NunitoSans_7pt-Regular.ttf"),
+    NunitoSansBold: require("../assets/fonts/NunitoSans_7pt-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <View>
-      <Text>Home</Text>
+      <Text style={{fontFamily: 'NunitoSansBold'}}>Home</Text>
     </View>
   );
 }
